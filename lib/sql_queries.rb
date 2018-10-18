dc4dd2cfb2400032505a3a8a5b8302d072747d96
Richard Cho <richardcho100@gmail.com>
2018-10-18 16:49:48 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -9,7 +9,7 @@
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_name
 # "SELECT project_id, SUM(amount) FROM pledges INNER JOIN projects ON pledges.project_id = projects.id
 # GROUP BY user_id"
-  "SELECT projects.title FROM projects ORDER BY projects.title"
+  "SELECT projects.title, pledge.amount FROM projects ORDER BY projects.title"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
