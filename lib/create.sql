4a716bf0bfbc0629930bd2223781844b61680643
Richard Cho <richardcho100@gmail.com>
2018-10-18 19:06:44 +0000

    automatically backed up by learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,8 +1,3 @@
--- **Create a schema based on the following information:**
--- - A project has a title, a category, a funding goal, a start date, and an end date.
--- - A user has a name and an age
--- - A pledge has an amount. It belongs to a user, and it also belongs to a project.
-
 CREATE TABLE projects (
   id INTEGER PRIMARY KEY,
   title TEXT,
